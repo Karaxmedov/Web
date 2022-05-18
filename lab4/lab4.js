b428b854601212d2db5322c6df9c329521160ebf
Nicat Qarəhmədov <69952695+Karaxmedov@users.noreply.github.com>
2022-05-18 10:18:05 +0400

    Add files via upload

---
+++ lab4/lab4.js
@@ -0,0 +1,22 @@
+var a, b, y, x, w;
+a = parseFloat(prompt("Enter a ", ""));
+b = parseFloat(prompt("Enter b ", ""));
+x = parseFloat(prompt("Enter x ", ""));
+y = b * Math.pow(Math.tan(x), 2) * x - a / Math.pow(Math.sin(x / a), 2);
+w = a * Math.pow(Math.E, -Math.sqrt(a)) * Math.cos((b * x) / a) + Math.abs(y);
+
+alert("y=" + y);
+alert("w=" + w);
+
+function cond() {
+  var a, y, x, w;
+  a = Number(document.form.a.value);
+  x = Number(document.form.x.value);
+  if (x > 1) {
+    y = Math.tan(x + 1);
+  } else if (x <= 1) y = Math.abs(a * x);
+  else {
+  }
+
+  document.forma.y.value = y;
+}
